d3179dd8e5f8bd8998ccda8abd5a1d0832037650
stein_korsveien@msn.com <stein_korsveien>
2018-09-28 12:19:14 +0200

    Added test and code for EditableTimelist TimersDashboard

---
+++ src/components/TimersDashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default class TimersDashboard extends React.Component {
+    render()  {
+      return (
+        <div className = 'ui three column centered grid'>
+            <div className = 'column'>
+
+            </div>
+        </div> 
+
+      );
+    }
+}
